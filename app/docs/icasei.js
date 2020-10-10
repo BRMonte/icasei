a133f32754eaf229bd4e670a5a1962ece0ac84c3
bruno montebello <brunormontebello@hotmail.com>
2020-10-10 15:25:47 +0100

    Create store method

--- app/docs/icasei.js
+++ app/docs/icasei.js
@@ -1,3 +1,8 @@
 var icasei = {
-  store: function(options = {}){}
+  store: function(options = {}){
+    localStorage.setItem("id", options.id);
+    localStorage.setItem("url", document.URL);
+    localStorage.setItem("created_at", options.created_at)
+
+  }
 }
