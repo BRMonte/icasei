f9b3dfe82e0ab07ec0a1c1fc13dba5c6ecd3a116
bruno montebello <brunormontebello@hotmail.com>
2020-10-10 16:19:07 +0100

    Create fetch method

--- app/docs/icasei.js
+++ app/docs/icasei.js
@@ -2,7 +2,9 @@ var icasei = {
   store: function(options = {}){
     localStorage.setItem("id", options.id);
     localStorage.setItem("url", document.URL);
-    localStorage.setItem("created_at", options.created_at)
-
+    localStorage.setItem("created_at", options.created_at);
+  },
+  fetch: function(key){
+    return localStorage.getItem("key");
   }
 }
